5534cf6d2e11c36a8d069b0903ba68693a6d5c39
blickwinkle <1095400631@qq.com>
2023-08-23 17:18:45 +0800

    tmp

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,7 +1,11 @@
 #include "tcp_sender.hh"
+#include "buffer.hh"
 #include "byte_stream.hh"
 #include "tcp_config.hh"
+#include "tcp_sender_message.hh"
+#include "wrapping_integers.hh"
 
+#include <cstddef>
 #include <cstdint>
 #include <random>
 
@@ -9,69 +13,77 @@ using namespace std;
 
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) )
+  : ack_record_ { 0, 0 }
+  , msg_()
+  , isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) )
   , initial_RTO_ms_( initial_RTO_ms )
   , RTO_ms_( initial_RTO_ms )
-  , retransmission_timeout_counter_( 0 )
+  , timer_()
   , consecutive_retransmissions_( 0 )
-  , last_ack_received_( 0 )
-  , last_seqno_sent_( 0 )
-  , last_window_size_( 0 )
-  , local_buffer_idx_( 0 )
-  , needRetransmission_( false )
-  , has_syn_( false )
-  , has_fin_( false )
-  , has_send_fin_( false )
   , buffer_()
 {}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return this->buffer_.size() + !this->ack_record_.has_ack_syn_ + !this->ack_record_.has_ack_fin_;
+  return min( this->buffer_.size() - this->ack_record_.last_ack_received_ + 1,
+              max( this->ack_record_.last_ack_window_size_, (uint64_t)1 ) );
+  // uint64_t seqNo = this->msg_.empty()
+  //                    ? this->ack_record_.last_ack_received_
+  //                    : this->msg_.back().seqno.unwrap( this->isn_, this->ack_record_.last_ack_received_ )
+  //                        + this->msg_.back().sequence_length();
+  // uint64_t payload_idx = seqNo;
+  // if ( payload_idx > 0 )
+  //   payload_idx--;
+  // return this->buffer_.size() - payload_idx + seqNo == 0;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return this->retransmission_timeout_counter_;
+  return this->consecutive_retransmissions_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
   // Your code here.
-  if ( this->needRetransmission_ ) {
-    this->needRetransmission_ = false;
-    this->consecutive_retransmissions_++;
+  if ( this->timer_.isExpir() ) {
+    if ( this->calc_remain_wsize() != 0 ) {
+      this->consecutive_retransmissions_++;
+      this->RTO_ms_ *= 2;
+    }
     if ( this->consecutive_retransmissions_ > TCPConfig::MAX_RETX_ATTEMPTS ) {
       return {};
     }
-    uint64_t payload_size
-      = min( min( this->ack_record_.last_ack_window_size_, this->buffer_.size() ), TCPConfig::MAX_PAYLOAD_SIZE );
-    bool is_fin
-      = this->ack_record_.last_ack_received_ + payload_size >= this->buffer_.size() + this->local_buffer_idx_;
-    return this->construct_message(
-      this->ack_record_.last_ack_received_, payload_size, !this->ack_record_.has_ack_syn_, is_fin );
-  }
 
-  if ( this->last_seqno_sent_ >= this->buffer_.size() + this->local_buffer_idx_ ) {
+    this->timer_.close();
+    this->timer_.start( this->RTO_ms_ );
+    return this->msg_.front();
+  }
+  uint64_t seqNo = this->msg_.empty()
+                     ? this->ack_record_.last_ack_received_
+                     : this->msg_.back().seqno.unwrap( this->isn_, this->ack_record_.last_ack_received_ )
+                         + this->msg_.back().sequence_length();
+  uint64_t payload_idx = seqNo;
+  if ( payload_idx > 0 )
+    payload_idx--;
+  uint64_t payload_size = min(
+    min( this->calc_remain_wsize(), this->buffer_.size() >= payload_idx ? this->buffer_.size() - payload_idx : 0 ),
+    TCPConfig::MAX_PAYLOAD_SIZE );
+
+  if ( payload_size == 0 && seqNo != 0 )
     return {};
-  }
-  uint64_t payload_size = min( min( this->last_window_size_ - ( this->last_seqno_sent_ - this->last_ack_received_ ),
-                                    this->buffer_.size() - ( this->last_seqno_sent_ - this->local_buffer_idx_ ) ),
-                               TCPConfig::MAX_PAYLOAD_SIZE );
-  bool is_fin
-    = this->has_fin_ && this->last_seqno_sent_ + payload_size >= this->buffer_.size() + this->local_buffer_idx_;
 
-  TCPSenderMessage message
-    = this->construct_message( this->last_seqno_sent_, payload_size, !this->has_syn_, is_fin );
-  if ( !this->has_syn_ ) {
-    this->has_syn_ = true;
+  TCPSenderMessage message { .seqno = Wrap32::wrap( seqNo, this->isn_ ),
+                             .SYN = seqNo == 0,
+                             .payload
+                             = { payload_size != 0 ? this->buffer_.substr( payload_idx, payload_size ) : "" },
+                             // .FIN = payload_idx + payload_size >= buffer_.size(),
+                             .FIN = false };
+  this->msg_.push( message );
+  if ( !this->timer_.isTiming() ) {
+    this->timer_.start( this->RTO_ms_ );
   }
-  if ( is_fin ) {
-    this->has_send_fin_ = true;
-  }
-  this->last_seqno_sent_ += payload_size;
   return message;
 }
 
@@ -79,14 +91,19 @@ void TCPSender::push( Reader& outbound_stream )
 {
   // Your code here.
   // (void)outbound_stream;
-  read( outbound_stream, outbound_stream.bytes_buffered(), buffer_ );
-  this->has_fin_ = outbound_stream.is_finished();
+  string buf_;
+  read( outbound_stream, outbound_stream.bytes_buffered(), buf_ );
+  this->buffer_ += buf_;
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
   // Your code here.
-  return { Wrap32::wrap( this->last_ack_received_ + this->has_syn_, this->isn_ ), false, {}, false };
+  uint64_t seqNo = this->msg_.empty()
+                     ? this->ack_record_.last_ack_received_
+                     : this->msg_.back().seqno.unwrap( this->isn_, this->ack_record_.last_ack_received_ )
+                         + this->msg_.back().sequence_length();
+  return { Wrap32::wrap( seqNo, this->isn_ ), false, {}, false };
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
@@ -94,49 +111,51 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
   // Your code here.
   uint64_t ackno = 0;
   if ( msg.ackno.has_value() ) {
-    ackno = msg.ackno.value().unwrap( this->isn_, this->last_seqno_sent_ );
+    ackno = msg.ackno.value().unwrap( this->isn_, this->ack_record_.last_ack_received_ );
   }
-  if ( ackno <= this->last_ack_received_ ) {
+  if ( ackno <= this->ack_record_.last_ack_received_ ) {
     return;
   }
-  this->last_ack_received_ = ackno;
-  this->last_window_size_ = msg.window_size;
+  this->ack_record_.last_ack_received_ = ackno;
+  this->ack_record_.last_ack_window_size_ = msg.window_size;
   this->consecutive_retransmissions_ = 0;
   this->RTO_ms_ = this->initial_RTO_ms_;
-  this->retransmission_timeout_counter_ = 0;
-  this->needRetransmission_ = false;
+
   this->GC_buffer();
+  if ( !this->msg_.empty() ) {
+    this->timer_.start( this->RTO_ms_ );
+  }
   return;
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
   // Your code here.
-  this->retransmission_timeout_counter_ += ms_since_last_tick;
-  if ( this->retransmission_timeout_counter_ >= this->RTO_ms_ ) {
-    this->retransmission_timeout_counter_ = 0;
-    this->consecutive_retransmissions_++;
-    needRetransmission_ = true;
-    this->RTO_ms_ *= 2;
+  this->timer_.addTime( ms_since_last_tick );
+  return;
+}
+
+void TCPSender::GC_buffer()
+{
+  if ( this->msg_.empty() )
+    return;
+  while ( !msg_.empty() ) {
+    auto tmpSeq = msg_.front().seqno.unwrap( this->isn_, this->ack_record_.last_ack_received_ );
+    if ( tmpSeq + msg_.front().sequence_length() > this->ack_record_.last_ack_received_ ) {
+      break;
+    }
+    msg_.pop();
   }
   return;
 }
 
-TCPSenderMessage TCPSender::construct_message( uint64_t seqno, uint64_t size, bool is_syn, bool is_fin ) const
+uint64_t TCPSender::calc_remain_wsize() const
 {
-  TCPSenderMessage message;
-  message.seqno = Wrap32::wrap( seqno + this->has_syn_, this->isn_ );
-  message.SYN = is_syn;
-  message.FIN = is_fin;
-  message.payload = Buffer( this->buffer_.substr( seqno - this->local_buffer_idx_, size ) );
-  return message;
-}
-void TCPSender::GC_buffer()
-{
-  if ( this->buffer_.empty() || this->local_buffer_idx_ >= this->last_ack_received_ ) {
-    return;
+  if ( this->msg_.empty() ) {
+    return this->ack_record_.last_ack_window_size_;
   }
-  this->buffer_ = this->buffer_.substr( this->last_ack_received_ - this->local_buffer_idx_ );
-  this->local_buffer_idx_ = this->last_ack_received_;
-  return;
+  return this->ack_record_.last_ack_window_size_
+         - ( ( this->msg_.back().seqno.unwrap( this->isn_, this->ack_record_.last_ack_received_ )
+               + this->msg_.back().sequence_length() )
+             - this->ack_record_.last_ack_received_ );
 }
